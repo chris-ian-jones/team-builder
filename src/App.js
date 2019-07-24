6b349c51134b9f14a01ce7065249713eccf3eb0b
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-24 20:47:45 +0100

    WIP: setup first draft of input menu

--- src/App.js
+++ src/App.js
@@ -1,24 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import TeamMembersList from './TeamMembersList';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <label>
+          Name
+          <input />
+        </label>
+        <label>
+          Email
+          <input />
+        </label>
+        <label>
+          Role
+          <input />
+        </label>
+          <input type="button" value="Submit"/>
+      </form>
     </div>
   );
 }
