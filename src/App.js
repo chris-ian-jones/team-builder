a5bb91234862513b5a789809e4a288e2330b2a1b
Chris Jones <chrisjones@Chriss-MacBook-Pro.local>
2019-07-25 01:46:18 +0100

    WIP: commit

--- src/App.js
+++ src/App.js
@@ -1,57 +1,15 @@
 import React, { useState } from 'react';
 import './App.css';
 import TeamMemberList from './TeamMemberList'
+import Form from './Form';
 
 function App() {
-  
-  const [formState, setFormState] = useState({
-    name: '',
-    email: '',
-    role: ''
-  })
 
-  const [personArray, setPersonArray] = useState([])
+  const [memberToEdit, setMemberToEdit] = useState()
 
-  const submitHandler = (event) => {
-    event.preventDefault()
-    setPersonArray(personArray.concat(formState))
-
-  }
-
-  console.log('personArray', personArray)
   return (
     <div className="App">
-      <form onSubmit={submitHandler}>
-        <label>
-          Name
-          <input 
-            onChange={event => {
-              setFormState({ ...formState, name: event.target.value})
-            }}
-          />
-        </label>
-        <label>
-          Email
-          <input
-             onChange={event => {
-              setFormState({ ...formState, email: event.target.value})
-            }}
-          />
-        </label>
-        <label>
-          Role
-          <input 
-            onChange={event => {
-              setFormState({ ...formState, role: event.target.value})
-            }}
-          />
-        </label>
-          <button>Submit</button>
-      </form>
-      {personArray.map(person => (
-        <TeamMemberList key={person.email} person={person}/>
-      ))}
-      
+      <Form />
     </div>
   );
 }
--- src/TeamMemberList.js
+++ src/TeamMemberList.js
@@ -4,10 +4,10 @@ function TeamMemberList({ person }) {
     const { name, email, role } = person;
     return (
       <div>
+          <h4>Person Card</h4>
           <p>Name: {name}</p>
           <p>Email: {email}</p>
           <p>Role: {role}</p>
-       
       </div>
     );
   }
